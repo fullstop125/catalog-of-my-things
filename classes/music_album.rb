2964368f4fde5fd83fb57945527b5011213f8bb9
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-11-15 23:35:02 +0430

    Add intializ def in music_album.rb

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -2,5 +2,8 @@ require_relative './items'
 class MusicAlbum < Item
   attr_accessor :on_spotify, :name
   attr_reader :items
+  def initialize(publish_date, on_spotify, name)
+    super(publish_date)
 
+  end
 end
