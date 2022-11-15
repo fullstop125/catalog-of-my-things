576224deafc196326702946ff50f9d0c6458ef92
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-11-15 23:59:05 +0430

    fix linters error

--- modules/preserver_module.rb
+++ modules/preserver_module.rb
@@ -1,13 +1,13 @@
-require 'json'
+require `json`
 
 module PreserverModule
   def save_data_as_json(data, file_name)
-    File.write("./json/#{file_name}.json", JSON.generate(data)) if data.any?
+    File.write(`./json/#{file_name}.json`, JSON.generate(data)) if data.any?
   end
 
   def load_file(file_name)
-    if File.exist?("./json/#{file_name}.json")
-      JSON.parse(File.read("./json/#{file_name}.json"))
+    if File.exist?(`./json/#{file_name}.json`)
+      JSON.parse(File.read(`./json/#{file_name}.json`))
     else
       []
     end
