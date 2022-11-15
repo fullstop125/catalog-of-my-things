0f84625ad241a44f19c3092138d842a5fd101a43
Juan Diaz <jfdiaza@unal.edu.co>
2022-11-15 15:00:19 -0500

    create author and game classes with attributes and methods

---
+++ game.rb
@@ -0,0 +1,17 @@
+require './item'
+require 'date'
+
+class Game < Item
+
+  def initialize(genre, author, source, label, publish_date, multiplayer, last_played_at)
+    super(genre, author, source, label, publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse last_played_at
+  end
+
+  def can_be_archived?
+    two_years_ago = Date.today - 730
+    super && (two_years_ago>@last_played_at)
+  end
+
+end
