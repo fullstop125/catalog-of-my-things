6ce77e76dac6cf1d9ebaf8939268c4a30d590c4e
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-11-15 23:43:40 +0430

    fix linters errors

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,7 +1,8 @@
-require_relative './items'
+require './items'
 class MusicAlbum < Item
   attr_accessor :on_spotify, :name
   attr_reader :items
+
   def initialize(publish_date, on_spotify, name)
     super(publish_date)
     @on_spotify = on_spotify
