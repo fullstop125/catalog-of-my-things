6faaa067c0bcba8973c4f4373ab454fd845c8bf1
fullstop125 <hassanmoses125@gmail.com>
2022-11-16 14:47:51 +0300

    add book class with methods

---
+++ classes/book.rb
@@ -0,0 +1,14 @@
+require relative 'items'
+class Book
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
