0751923484d02985bd65a2de0278131c63bed1d6
fullstop125 <hassanmoses125@gmail.com>
2022-11-16 15:03:31 +0300

    fix linters

--- classes/label.rb
+++ classes/label.rb
@@ -1,16 +1,15 @@
 require_relative 'book'
 class Label
+  attr_accessor :title, :items, :color, :label
 
-    attr_accessor :title, :items, :color, :label
+  def initialize(title, color)
+    @title = title
+    @color = color
+    @items = []
+  end
 
-    def initialize(title, color)
-        @title = title
-        @color = color
-        @items = []
-    end
-
-    def add_item(item)
-        @items.push(item) unless @items.include?(item)
-        item.label = self
-
+  def add_item(item)
+    @items.push(item) unless @items.include?(item)
+    item.label = self
+  end
 end
