0d1038779a607ff69e85605f8c7e0d794164e31d
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-11-18 03:27:23 +0430

    Add book title

--- classes/books.rb
+++ classes/books.rb
@@ -1,10 +1,11 @@
 require_relative './items'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state
+  attr_accessor :title, :publisher, :cover_state
 
-  def initialize(publisher, publish_date, cover_state)
+  def initialize(title, publisher, publish_date, cover_state)
     super(publish_date)
+    @title = title
     @publisher = publisher
     @cover_state = cover_state
   end
