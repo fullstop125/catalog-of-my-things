c90d7164fb784057b7a00c8a144a83f9c4f4cb4b
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-11-18 03:26:42 +0430

    Add the "Add_authtor" method and book title

--- modules/book_module.rb
+++ modules/book_module.rb
@@ -5,14 +5,16 @@ module BookModule
   end
 
   def add_new_book_details
+    add_new_author_details
     puts "\n-------------------------------"
     puts "\nENTER BOOK DETAILS\n\n"
 
+    title = capture_input('Input Book name')
     publisher = capture_input('Input publisher name')
     publish_date = capture_input('Input published date[yyyy/mm/dd]')
     cover_state = capture_input('Input cover state eg:(good or bad)')
 
-    add_book([publisher, publish_date, cover_state])
+    add_book([title, publisher, publish_date, cover_state])
     add_new_label_details
   end
 
@@ -24,6 +26,18 @@ module BookModule
 
     add_label([title, color])
     puts "\nNew Book and Label Added!"
+    puts "\n-------------------------------"
+  end
+
+  def add_new_author_details
+    puts "\n-------------------------------"
+    puts "\nENTER AUTHOR DETAILS\n\n"
+
+    first_name = capture_input('Input author First name')
+    last_name = capture_input('Input last name')
+
+    add_author([first_name, last_name])
+    puts "\n New Author Added"
     puts "\n-------------------------------"
   end
 end
