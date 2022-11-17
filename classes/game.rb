b52d4555d88ad4af29a4fda533a8f1a0159ef14e
Juan Diaz <jfdiaza@unal.edu.co>
2022-11-17 02:59:32 -0500

    update game class

--- classes/game.rb
+++ classes/game.rb
@@ -3,8 +3,9 @@ require 'date'
 
 class Game < Item
 
-  def initialize(publish_date, multiplayer, last_played_at)
+  def initialize(game_name, publish_date, multiplayer, last_played_at)
     super(publish_date)
+    @game_name = game_name
     @multiplayer = multiplayer
     @last_played_at = Date.parse last_played_at
   end
